05a6b264ea04b216ce0e8d1163f582e83e097693
Vladislav Milrat <vrooog@gmail.com>
2026-03-16 13:48:05 +0300

    update CartItemsHTML components

--- src/components/cart/cartItems/CartItemsHTML.tsx
+++ src/components/cart/cartItems/CartItemsHTML.tsx
@@ -1,12 +1,14 @@
-import { useAppSelector } from '@/shared/redux'
-import { Link } from 'react-router-dom'
 import Button from '@/components/button/Button'
 import btn from '@/components/button/style.module.scss'
-import CartItem from './cartItem/CartItem'
+import { useAppSelector } from '@/shared/redux'
+import { Link, useLocation } from 'react-router-dom'
 import style from '../style.module.scss'
+import CartItem from './cartItem/CartItem'
 
 export const CartItemsHTML = () => {
 	const { items, totalCount, totalAmount } = useAppSelector(state => state.cart)
+	const location = useLocation()
+
 	if (items.length === 0) {
 		return (
 			<section className={style.items}>
@@ -15,32 +17,34 @@ export const CartItemsHTML = () => {
 		)
 	}
 
-	if (items.length > 0) {
-		return (
-			<>
-				<section className={style.items}>
-					{items.map((item, index) => {
-						return (
-							<CartItem
-								key={index}
-								item={item}
-							/>
-						)
-					})}
-				</section>
+	return (
+		<>
+			<section className={style.items}>
+				{items.map((item, index) => {
+					return (
+						<CartItem
+							key={index}
+							item={item}
+						/>
+					)
+				})}
+			</section>
 
-				<section className={style.info}>
-					<p>Итого: {totalCount}</p>
-					<p>Стоимость: {totalAmount}</p>
-				</section>
+			<section className={style.info}>
+				<p>Итого: {totalCount}</p>
+				<p>Стоимость: {totalAmount}</p>
+			</section>
 
+			{location.pathname !== '/order' ? (
 				<Link
 					to="order"
 					className={btn.btn}
 				>
 					<Button title="Оформить заказ" />
 				</Link>
-			</>
-		)
-	}
+			) : (
+				<Button title="Заполните форму" />
+			)}
+		</>
+	)
 }
