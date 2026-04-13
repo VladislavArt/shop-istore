54cbdc5246b4d3efe3ca162c836df73385e90ff1
Vladislav Milrat <vrooog@gmail.com>
2026-04-13 20:00:29 +0300

    update CartItemHTML and Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,21 +1,31 @@
+import { type ElementType } from 'react'
 import btn from './style.module.scss'
 
 type Button = {
 	title: string
 	disabled?: boolean
 	onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+	as?: ElementType
+	[key: string]: unknown
 }
 
-function Button( {title, disabled = false, onClick}: Button ) {
+function Button({
+	title,
+	disabled = false,
+	onClick,
+	as: Component = 'button',
+	...props
+}: Button) {
 	return (
-		<button 
+		<Component
 			className={btn.btn}
 			disabled={disabled}
 			onClick={onClick}
+			{...props}
 		>
 			{title}
-		</button>
-	);
+		</Component>
+	)
 }
 
 export default Button
--- src/components/cart/cartItems/CartItemsHTML.tsx
+++ src/components/cart/cartItems/CartItemsHTML.tsx
@@ -1,5 +1,4 @@
 import Button from '@/components/button/Button'
-import btn from '@/components/button/style.module.scss'
 import { useAppSelector } from '@/shared/redux'
 import { Link, useLocation } from 'react-router-dom'
 import style from '../style.module.scss'
@@ -7,6 +6,7 @@ import CartItem from './cartItem/CartItem'
 
 export const CartItemsHTML = () => {
 	const { items, totalCount, totalAmount } = useAppSelector(state => state.cart)
+
 	const location = useLocation()
 
 	if (items.length === 0) {
@@ -20,10 +20,10 @@ export const CartItemsHTML = () => {
 	return (
 		<>
 			<section className={style.items}>
-				{items.map((item, index) => {
+				{items.map(item => {
 					return (
 						<CartItem
-							key={index}
+							key={item.id}
 							item={item}
 						/>
 					)
@@ -36,12 +36,11 @@ export const CartItemsHTML = () => {
 			</section>
 
 			{location.pathname !== '/order' ? (
-				<Link
-					to="order"
-					className={btn.btn}
-				>
-					<Button title="Оформить заказ" />
-				</Link>
+				<Button
+					as={Link}
+					to="/order"
+					title="Оформить заказ"
+				/>
 			) : (
 				<Button title="Заполните форму" />
 			)}
