c2351f6e7c6abc7125f758c98c28aae72e6b81ba
Vladislav Milrat <vrooog@gmail.com>
2026-04-23 15:06:39 +0300

    update styles

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,4 @@
-import '@/styles/reset.css'
-import '@/styles/index.css'
+import '@/styles/index.scss'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
